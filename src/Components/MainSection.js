3eb3ecbe35260bc55afaf00a66df5feb7fe3a8a8
shehap ghazal <sh.s.ghazal2003@gmail.com>
2025-09-27 13:27:45 +0300

    Update

--- src/Components/MainSection.js
+++ src/Components/MainSection.js
@@ -3,6 +3,7 @@ import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
+import Divider from '@mui/material/Divider';
 import TaskForm from './TaskForm';
 import TaskBox from './TaskBox';
 import { useState  , useContext} from 'react';
@@ -28,8 +29,8 @@ export default function  MainSection() {
     };
     return (
         <Container p={2} maxWidth = "md" sx={{padding : "8px", margin : "40px 10px" , borderRadius : "15px" , backgroundColor : "white" , boxShadow: "0 12px 24px rgba(0, 0, 0, 0.2)"}}>
-            <div style={{backgroundColor :"re"}}>
-                <h1 style={{fontSize : "50px", color:"#B22222"}}>مهامى</h1>
+            <div>
+                <h1 style={{fontSize : "50px", color:"#B22222"}}>مهامى</h1><Divider variant="middle" component="hr" sx={{mb:2 , width:"50%" , margin :" 8px auto" , height : "1px" , backgroundColor : "#B22222"}} />
                 <ToggleButtonGroup
                     color="error"
                     value={alignment}
@@ -37,6 +38,7 @@ export default function  MainSection() {
                     onChange={handleChange}
                     aria-label="Platform"
                     size='large'
+                    sx={{mt:2}}
                     >
                     <ToggleButton value="غير منجز">غير منجز</ToggleButton>
                     <ToggleButton value="منجز">منجز</ToggleButton>
