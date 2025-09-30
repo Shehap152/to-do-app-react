1438a09285a482d69f062a1b80c91fe61c4c0319
shehap ghazal <sh.s.ghazal2003@gmail.com>
2025-09-30 13:43:07 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import './App.css';
-import  MainSection from './Components/MainSection'
-import { TaskProvider } from './Contexts/TaskContext';
-import {AlertEditProvider} from './Contexts/AlertEditContext'
-import { AlertProvider } from './Contexts/AlertContext';
-import AlertEdit from './Components/AlertEdit';
+import "./App.css";
+import MainSection from "./Components/MainSection";
+import { TaskProvider } from "./Contexts/TaskContext";
+import { AlertEditProvider } from "./Contexts/AlertEditContext";
+import { AlertProvider } from "./Contexts/AlertContext";
+import AlertEdit from "./Components/AlertEdit";
 
-function App() {  
+function App() {
   return (
     <TaskProvider>
       <AlertEditProvider>
-          <div className="App">
-            <AlertProvider>
-              <MainSection/>
-              <AlertEdit/>
-            </AlertProvider>
-          </div>
+        <div className="App">
+          <AlertProvider>
+            <MainSection />
+            <AlertEdit />
+          </AlertProvider>
+        </div>
       </AlertEditProvider>
     </TaskProvider>
   );
-} 
+}
 
 export default App;
